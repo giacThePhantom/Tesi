2205cfed49951c76e88d803c0a1c35a2c1fc7442
giacomo fantoni <giacomo.fantoni@studenti.unitn.it>
2021-07-18 20:11:24 +0200

    Added bibliography compilation to build script

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -1,9 +1,14 @@
 echo "Building"
 
 PROJECTHOME="$HOME/Documents/Universita/Elaborato_finale/"
-NAME="cognome_nome_laurea_aa.tex"
+NAME="cognome_nome_laurea_aa"
 
 cd $PROJECTHOME
 pwd
 
-pdflatex -output-directory=./build $NAME
+pdflatex -halt-on-error -output-directory=./build "$NAME.tex"
+[ $? == 0 ] || exit 1
+bibtex "./build/$NAME.aux"
+[ $? == 0 ] || exit 1
+pdflatex -output-directory=./build "$NAME.tex" 2>/dev/null >/dev/null
+pdflatex -output-directory=./build "$NAME.tex" 2>/dev/null >/dev/null
